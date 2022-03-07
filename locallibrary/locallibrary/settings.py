6586abdd4b33e124aaaf73ca2e5d693218a97c23
puntoquadrato <valerio.gianella@gmail.com>
2022-03-07 22:11:24 +0100

    updated allowed hosts

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['limitless-savannah-11991.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
